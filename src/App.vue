a8a826d6e3c69055ecebe65fe2e4482a032f9f28
unknown <AlgorithmofAction>
2025-02-18 18:12:22 +0300

    components

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <script setup>
+import TodoAdd from './components/TodoAdd.vue';
 </script>
 
 <template>
-  <h1 class="text-2xl font-bold text-center mt-5 mb-14">Список Задач на Vue</h1>
 
-  <form class="form rounded-lg w-sm">
-    <label class="flex gap-3 items-baseline justify-center">
-      <input class="outline-none" placeholder="Введите задачу" type="text">
-      <button class="btn mt-3 cursor-pointer" type="submit">Добавить задачу</button>
-    </label>
-  </form>
+<TodoAdd />
 </template>
 
 <style scoped>
